d08a9c073993fbcba6e9312c55ddaf8cc2eebe33
suineglive13 <lex667@mail.ru>
2016-05-20 13:49:52 +0300

    modified:   etc/nginx.conf 	new file:   hello.py 	modified:   init.sh

---
+++ hello.py
@@ -0,0 +1,10 @@
+def app(environ, start_response):
+  params = environ["QUERY_STRING"]
+  body = "\n".join(params.split("&"))
+  status = "200 OK"
+  headers = [
+          ("Content-Type", "text/plain"),
+          ("Content-Length", str(len(body)))
+  ]
+  start_response(ststus, headers)
+  return iter([body])
