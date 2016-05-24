38cd6fe195aa25fd33358ee54b5f4c5ceb13ace3
suineglive13 <lex667@mail.ru>
2016-05-24 15:41:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ls -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
